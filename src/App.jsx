282ceb27e070b067a4109b875b63457ff50a1134
HackerMgPyone <arkarminmyat121@gmail.com>
2024-03-04 00:11:36 +0630

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { AddToCart } from './components/AddToCart'
+import { Stars } from './components/Stars'
+
+function App() {
+
+  
+
+  return (
+    <>
+      <AddToCart></AddToCart>
+      <Stars></Stars>
+    </>
+  )
+}
+
+export default App
